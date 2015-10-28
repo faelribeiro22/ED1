1898e02b5f9057e1c5e46c40def3782bccb00ddf
Rafael Ribeiro <faelribeirodesousa@gmail.com>
2015-10-28 12:12:52 -0200

    Comentando cliente.cpp

--- fila_dinamica/cliente.cpp
+++ fila_dinamica/cliente.cpp
@@ -9,12 +9,14 @@ int main(){
     int x;
     fi = cria_Fila();
     x = tamanho_Fila(fi);
+    //testando criação da fila e verificação se fila esta vazia
     printf("Tamanho da fila eh %d\n",x);
     if(Fila_vazia(fi)){
         printf("Fila esta vazia!\n");
     }else{
         printf("fila nao esta vazia!\n");
     }
+    //inserção
     x = insere_Fila(fi, valores);
     x = tamanho_Fila(fi);
     printf("Tamanho da fila eh %d\n",x);
