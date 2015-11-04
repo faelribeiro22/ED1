d7add52f59580ef6042976fee01cd0b994ce6f6a
Rafael Ribeiro <faelribeirodesousa@gmail.com>
2015-11-04 12:49:10 -0200

    Correção do código

--- 1030/main.cpp
+++ 1030/main.cpp
@@ -20,79 +20,54 @@ Lista* cria_lista(){
     return l;
 }
 
-int lista_vazia(Lista* l){
-    return l->qtd == 0;
-}
-
 int insere_vitima(Lista* l, int num){
-    Pessoa *vitima = (Pessoa*)malloc(sizeof(Pessoa));
-    Pessoa *aux = l->head;
-    if(lista_vazia(l)){
+    int i;
+    Pessoa *p;
+    for(i=1;i<=num;i++){
+        Pessoa *vitima = (Pessoa*)malloc(sizeof(Pessoa));
+        vitima->num = i;
+        vitima->prox = NULL;
         l->qtd++;
-        vitima->prox = vitima;
-        vitima->num = num;
-        l->head = vitima;
-        return 1;
-    }else{
-        while(aux->prox != l->head){
-            aux = aux->prox;
+        if(i != 1){
+            l->head->prox = vitima;
+            l->head = vitima;
+        }else{
+            l->head = vitima;
+            p = l->head;
         }
-        l->qtd++;
-        vitima->num = num;
-        vitima->prox = l->head;
-        aux->prox = vitima;
-        return 1;
+        if(i == num){
+            vitima->prox = p;
+            l->head = p;
+        }
     }
 }
 
-Pessoa* busca(Lista* l, int x){
-    Pessoa *vitima = l->head;
+void busca(Lista* l, int x){
+    Pessoa *vitima = l->head,*p;
     int cont;
     for(cont=1;cont<x;cont++){
+        p = vitima;
         vitima = vitima->prox;
     }
-    return vitima;
+    l->head = vitima->prox;
+    l->qtd--;
+    p->prox = vitima->prox;
+    free(vitima);
 }
 
-void mata_vitima(Lista* l, Pessoa* vitima){
-    Pessoa* aux = l->head,*p;
-    if(vitima == l->head){
-        while(aux->prox != l->head){
-            aux = aux->prox;
-        }
-        p = l->head;
-        aux->prox = p->prox;
-        l->head = aux->prox;
-        free(p);
-        l->qtd--;
-    }else{
-        while(aux != vitima){
-        p = aux;
-        aux = aux->prox;
-        }
-        p->prox = aux->prox;
-        free(aux);
-        l->head = p->prox;
-        l->qtd--;
-    }
-}
 
 int main(){
-    Pessoa *vitima;
     int casos,k,n;
     scanf("%d",&casos);
     for(int i = 1; i <= casos; i++){
         Lista *l = cria_lista();
         scanf("%d",&n);
-        for(int j = 1; j<=n; j++){
-            insere_vitima(l,j);
-        }
+        insere_vitima(l,n);
         scanf("%d",&k);
         while(l->qtd != 1){
-            vitima = busca(l,k);
-            mata_vitima(l,vitima);
+            busca(l,k);
         }
         printf("Case %d: %d\n",i,l->head->num);
-    }
+        }
     return 0;
 }
