8a75c96a62d75b7273cf5ffe66147914f9b2b856
Rafael Ribeiro <faelribeirodesousa@gmail.com>
2015-11-19 13:37:36 -0200

    fila estatica

--- 1548/main.cpp
+++ 1548/main.cpp
@@ -1,65 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX 1000
 
 struct aluno{
     int numero;
     int nota;
-    struct aluno *prox;
 };
-
 typedef struct aluno Aluno;
 
 struct fila{
-    int qtd;
-    struct aluno *head;
+    int inicio, fim,total;
+    Aluno alunos[MAX];
 };
-
 typedef struct fila Fila;
 
 Fila* criaFila(){
     Fila* f = (Fila*)malloc(sizeof(Fila));
-    if(f == NULL)
-        exit(1);
-    f->qtd = 0;
-    f->head = NULL;
+    f->inicio = 0;
+    f->fim = 0;
+    f->total = 0;
     return f;
 }
 
-int insere(Fila* l, int num, int nota){
-    Aluno* p = (Aluno*)malloc(sizeof(Aluno));
-    p->numero = num;
-    p->nota = nota;
-    if(l->qtd == 0){
-        l->head = p;
-        p->prox = NULL;
-        l->qtd++;
-    }else{
-        p->prox = l->head;
-        l->head = p;
-        l->qtd++;
-    }
-    return 1;
+void esvaziaFila(Fila* f){
+    f->inicio = 0;
+    f->fim = 0;
+    f->total = 0;
+    return;
 }
 
-void exibe(Fila *f){
-    Aluno *aux = f->head;
-    while(aux != NULL){
-        printf("Aluno: %d Nota: %d\n",aux->numero,aux->nota);
-        aux = aux->prox;
+int IsEmpty(Fila* f){
+    return(f->total == 0);
+}
+
+int IsFull(Fila* f){
+    return(f->total == MAX-1);
+}
+
+int entraFila(Fila *f,Aluno* p){
+    if(!IsFull(f)){
+        f->total++;
+        f->alunos[f->fim] = *p;
+        f->fim = (f->fim+1)%MAX;
+        return 1;
     }
+    return 0;
+}
+
+Fila* copiaFila(Fila* f){
+    Fila *copia = criaFila();
+    int i;
+    for(i = 0;i<f->total;i++){
+        entraFila(copia,&f->alunos[i]);
+    }
+    return copia;
+}
+
+Fila* ordenaFila(Fila* f){
+    Fila* ordenada = criaFila();
+    Fila *p = copiaFila(f);
+    int i,j,maior;
+    Aluno temp;
+    for(i = f->inicio; i<f->fim;i++){
+        maior = i;
+        for(j = i+1; j<f->fim;j++){
+            if(f->alunos[j].nota > f->alunos[maior].nota){
+                maior = j;
+            }
+        }
+        entraFila(ordenada,&p->alunos[maior]);
+        temp = p->alunos[maior];
+        p->alunos[maior] = p->alunos[i];
+        p->alunos[i] = temp;
+    }
+    return ordenada;
+}
+
+int verifica(Fila* f, Fila* ordenada){
+    int c = 0,i;
+    for(i=0;i<f->fim;i++){
+        if(f->alunos[i].numero == ordenada->alunos[i].numero)
+            c++;
+    }
+    return c;
 }
 
 int main(){
+    int testes,alunos,notas,c;
+    Aluno aluno;
     Fila* f = criaFila();
-    int testes,alunos,notas;
+    Fila* ordenada;
     scanf("%d",&testes);
     while(testes != 0){
         scanf("%d",&alunos);
         for(int i = 0; i<alunos; i++){
             scanf("%d",&notas);
-            insere(f,i+1,notas);
+            aluno.numero = i+1;
+            aluno.nota = notas;
+            entraFila(f,&aluno);
         }
-        exibe(f);
+        ordenada = ordenaFila(f);
+        c = verifica(f,ordenada);
+        printf("%d\n",c);
+        esvaziaFila(f);
         testes--;
     }
     return 0;
